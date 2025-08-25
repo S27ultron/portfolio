9f72a00bd0ed68ef42e51f38c0c7821b771a2fbf
shubham <shubhamvitore@gmail.com>
2025-08-25 11:55:41 +0530

    404.html added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+sessionStorage.removeItem('redirect');
+window.history.replaceState(null, '', redirect);
+}
 root.render(
   <BrowserRouter>
     <App />
